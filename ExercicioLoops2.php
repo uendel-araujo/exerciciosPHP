873c1965e55cb404d6bab14898775ef41d718cee
Uendel Araujo <dendellaraujo@gmail.com>
2021-12-04 09:41:23 -0300

    Adicionado exercicio Loops 2

---
+++ ExercicioLoops2.php
@@ -0,0 +1,15 @@
+<?php
+
+ for($i = 0 ; $i < 1; $i++){
+    $tracinho = "";
+    echo $tracinho;
+    
+    for($j = 0; $j < 20; $j++) {
+       $tracinho .= "-";
+       echo $tracinho;
+       echo "<br/>";
+      
+    }
+  }
+
+?>
